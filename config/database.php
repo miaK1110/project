f39e811762996ce1b9c4c6bf1f75e4f7c95cdf1d
miaK1110 <sukithewestie1011@gmail.com>
2022-04-06 15:46:43 +0900

    changed collation

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'password' => env('DB_PASSWORD', 'gecko'),
             'unix_socket' => env('DB_SOCKET', '/var/lib/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_general_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
