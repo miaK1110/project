8fe83b872e32b7221edb4420129ff160799b049c
miaK1110 <sukithewestie1011@gmail.com>
2022-04-06 12:31:04 +0900

    fixed mail infomation

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'haikishare.mail@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'haiki-share'),
     ],
 
     /*
